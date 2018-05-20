9fb4f293450e198ff2632ddbe527c0da7a02004d
rockGithub <rock20110414@gmail.com>
2018-05-20 11:13:44 +0800

    adding webpack.config.js; then: npx webpack --config webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+// npx webpack --config webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
